6517dc1733841f4800f85453315e2b8141be6db1
James Lovejoy <jameslovejoy1@gmail.com>
2018-07-22 14:38:29 -0400

    Run in CMD window

--- main.go
+++ main.go
@@ -103,7 +103,7 @@ func startAMD(address string) (*exec.Cmd, io.ReadCloser, error) {
 		return nil, nil, err
 	}
 
-	cmd := exec.Command("./miners/AMD/lyclMiner015/lyclMiner.exe", "lycl.conf")
+	cmd := exec.Command("cmd", "/C", "start", "./miners/AMD/lyclMiner015/lyclMiner.exe", "lycl.conf")
 
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
@@ -119,7 +119,7 @@ func startAMD(address string) (*exec.Cmd, io.ReadCloser, error) {
 }
 
 func startNVIDIA(address string) (*exec.Cmd, io.ReadCloser, error) {
-	cmd := exec.Command("./miners/NVIDIA/ccminer-x64.exe", "-o", "stratum+tcp://p2proxy.vertcoin.org:9171", "-u", address, "-p", "x")
+	cmd := exec.Command("cmd", "/C", "start", "./miners/NVIDIA/ccminer-x64.exe", "-o", "stratum+tcp://p2proxy.vertcoin.org:9171", "-u", address, "-p", "x")
 
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
