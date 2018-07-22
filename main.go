b9bc832cd5b161a7aeb39efa4f8b5d7ea46a2405
James Lovejoy <jameslovejoy1@gmail.com>
2018-07-22 15:06:46 -0400

    Specify ccminer algorithm

--- main.go
+++ main.go
@@ -129,7 +129,7 @@ func startAMD(address string) (*exec.Cmd, io.ReadCloser, error) {
 }
 
 func startNVIDIA(address string) (*exec.Cmd, io.ReadCloser, error) {
-	cmd := exec.Command("cmd", "/C", "start", "./miners/NVIDIA/ccminer-x64.exe", "-o", "stratum+tcp://p2proxy.vertcoin.org:9171", "-u", address, "-p", "x")
+	cmd := exec.Command("cmd", "/C", "start", "./miners/NVIDIA/ccminer-x64.exe", "-a", "lyra2v2", "-o", "stratum+tcp://p2proxy.vertcoin.org:9171", "-u", address, "-p", "x")
 
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
